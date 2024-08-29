a452195e37eba7219790c3b75be93606b39772e5
Mugisha Ivan Jalagatha <jalagatha@gmail.com>
2024-08-29 21:20:11 +0300

    added files

---
+++ index.js
@@ -0,0 +1,21 @@
+document.querySelector("#fetchButton").addEventListener("click", () => {
+  //
+  const url = "https://official-joke-api.appspot.com/random_joke";
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      document.querySelector(
+        "#displayJoke"
+      ).innerText = `${data.setup}-${data.punchline}`;
+    })
+
+    .catch((error) => {
+      document.querySelector("#displayJoke").innerText = "something went wrong";
+      console.error("Problem encountered", error);
+    });
+});
